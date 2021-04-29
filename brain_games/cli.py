7822cd12bcb619e9ae93999c5e7d080821fa7f90
Nikolai Poroshin <picknick91@gmail.com>
2021-04-29 15:43:51 +0300

    step 4 passed

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
+"""CLI to ask user for their name."""
 import prompt
 
 
 def welcome_user():
-    name = prompt.string("May I have your name? ")
-    print("Hello, {}!".format(name))
+    """Ask user for a name at launch."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+"""Main module."""
 
 from brain_games.cli import welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    """Print welcome message and ask user for a name."""
+    print('Welcome to the Brain Games!')
     welcome_user()
 
 
