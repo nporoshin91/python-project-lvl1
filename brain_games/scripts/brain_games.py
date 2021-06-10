cda023ad9c283536149d160df73b822490017a11
Nikolai Poroshin <picknick91@gmail.com>
2021-06-10 14:17:13 +0300

    fixed brain-games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 """Print welcome message."""
+from brain_games.cli import welcome_user
 
 
 def main():
-    """Print welcome message."""
+    """Print welcome message and call CLI to greet the user."""
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 
 if __name__ == '__main__':
